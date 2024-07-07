95cdbc0a01a202175ca33448eb7ec97a488edda1
Shantanu Jain <shantanujain1507@gmail.com>
2024-07-07 05:45:47 +0530

    mongodb Connection completed

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ const connectDB = async () => {
     try {
       await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+        useUnifiedTopology: true
       });
       console.log('MongoDB connected successfully');
     } catch (err) {
-      console.error(err.message);
+      console.error("ERROR in the connectDB() -> " + err.message);
       process.exit(1); // Exit process with failure
     }
   };
-
+connectDB(); //connections...
 
 
 server.listen(PORT,()=>console.log(`Server is running on ${PORT}`));
