afc8a4de1f09106fb2b2dcf9b8463271d3ee2753
Shantanu Jain <shantanujain1507@gmail.com>
2024-07-08 02:04:12 +0530

    redirect compeleted

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongoose= require('mongoose')
 const URL=require('./model.js')
+const { nanoid } = require('nanoid')
 
 require('dotenv').config()
 // console.log(process.env) 
 const PORT=process.env.PORT || 5000;
 const server=express();
 
-
+server.use(express.json()); // middleware for reading JSon Object
 //connect the mongoDb 
 const connectDB = async () => {
     try {
@@ -21,7 +22,38 @@ const connectDB = async () => {
       process.exit(1); // Exit process with failure
     }
   };
-connectDB(); //connections...
+connectDB(); //connections..    
+
+server.get('/',(req,res)=>{
+
+  return res.json({check: true,nanoID : nanoid(11)});
+})
+server.post('/',async (req,res)=>{
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const nanoID = nanoid(11);
+  let shortURL = process.env.BASE_URL + "/" + nanoID;
+  // console.log({'nanoID' :  nanoID, 'shortURL': shortURL});
+
+  await URL.create({
+    nanoId: nanoID,
+    shortUrl : shortURL,
+    originalUrl: body.url
+  })
+
+  
+  return res.json({ 'shortURL': shortURL });
+});
+
+server.get("/:nanoId", async (req, res) => {
+  const nanoId = req.params.nanoId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      nanoId
+    }
+  );
+  res.redirect(entry.originalUrl);
+});
 
 
 server.listen(PORT,()=>console.log(`Server is running on ${PORT}`));
--- model.js
+++ model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const urlSchema=new mongoose.Schema({ 
+    nanoId :{
+      type : 'string',
+      unique : true,
+      required : true
+    },
     shortUrl :{
         type : 'string',
         unique : true,
